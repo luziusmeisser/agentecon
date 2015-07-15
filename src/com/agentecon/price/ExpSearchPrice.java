d9867f6428dd0a2c7edb1f02af893cf75adaab21
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-07-15 17:35:52 +0200

    Same adaption speed as Ulrich Wolffgang (1.1) seems to work

--- src/com/agentecon/price/ExpSearchPrice.java
+++ src/com/agentecon/price/ExpSearchPrice.java
@@ -1,22 +1,19 @@
 package com.agentecon.price;
 
-import java.util.Random;
-
 import com.agentecon.stats.Numbers;
 
 public class ExpSearchPrice extends AdaptablePrice {
 
+	public static final double ADAPTION_SPEED = 1.1;
 	public static final double MAX_ADAPTION_FACTOR = 0.5;
 	public static final double MIN_ADAPTION_FACTOR = Numbers.EPSILON * 1000;
 
 	private double factor;
 	private boolean direction;
-	private double adaptionSpeed;
 	private int sameDirectionInARow;
 
-	public ExpSearchPrice(double initialFactor, Random rand) {
+	public ExpSearchPrice(double initialFactor) {
 		this.factor = initialFactor;
-		this.adaptionSpeed = rand.nextDouble() + 1.5;
 		this.sameDirectionInARow = 0;
 	}
 
@@ -25,13 +22,13 @@ public class ExpSearchPrice extends AdaptablePrice {
 		if (increase == direction) {
 			sameDirectionInARow++;
 			if (sameDirectionInARow >= 2) {
-				factor = Math.min(MAX_ADAPTION_FACTOR, factor * adaptionSpeed);
+				factor = Math.min(MAX_ADAPTION_FACTOR, factor * ADAPTION_SPEED);
 				sameDirectionInARow = 0;
 			}
 		} else {
 			sameDirectionInARow = 0;
 			direction = increase;
-			factor = Math.max(MIN_ADAPTION_FACTOR, factor / adaptionSpeed);
+			factor = Math.max(MIN_ADAPTION_FACTOR, factor / ADAPTION_SPEED);
 		}
 		return factor;
 	}
