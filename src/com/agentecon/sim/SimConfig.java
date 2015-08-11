3549f2b2378e3e46ee3076969ccb1db61140bf19
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-08-11 10:01:06 +0200

    No intraday iterations

--- src/com/agentecon/sim/SimConfig.java
+++ src/com/agentecon/sim/SimConfig.java
@@ -13,7 +13,7 @@ import com.agentecon.good.Good;
 public class SimConfig extends SimulationConfig {
 	
 	public static final boolean AGING = false;
-	private static final int DEFAULT_WOBBLES = 100;
+	private static final int DEFAULT_WOBBLES = 0;
 	
 	public static final double GOODS_PERSISTENCE = 1.0;
 	
