4cd353c179313b47dbc21e70236bc9382ba672d9
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-08-12 11:44:52 +0200

    1/256 mix-in

--- src/com/agentecon/firm/Firm.java
+++ src/com/agentecon/firm/Firm.java
@@ -162,7 +162,7 @@ public class Firm extends Agent implements IFirm, IPriceProvider {
 		if (profits > maxCashPayout){
 			return Math.max(0, maxCashPayout);
 		} else {
-			double weightedMean = (profits*63 + maxCashPayout)/64;
+			double weightedMean = (profits*255 + maxCashPayout)/256;
 			return Math.max(0, weightedMean);
 		}
 	}
