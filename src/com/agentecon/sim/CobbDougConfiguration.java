4ccb06518c56d7a03a01194ed6e7fa541163b90e
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-08-17 14:45:59 +0200

    12000 rounds

--- src/com/agentecon/sim/CobbDougConfiguration.java
+++ src/com/agentecon/sim/CobbDougConfiguration.java
@@ -20,7 +20,7 @@ import com.agentecon.price.PriceFactory;
 
 public class CobbDougConfiguration implements IConfiguration {
 
-	public static final int ROUNDS = 5000;
+	public static final int ROUNDS = 12000;
 	public static final int ITERATIONS = 0;
 
 	private int iteration = 0;
