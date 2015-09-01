9d0593ddd4502d435aa2bf3d8743f2c97db9a166
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-01 22:16:42 +0200

    randomized with small adaption factor

--- src/com/agentecon/verification/StolperSamuelson.java
+++ src/com/agentecon/verification/StolperSamuelson.java
@@ -79,7 +79,7 @@ public class StolperSamuelson {
 		SimConfig config = new SimConfig(3000, 23, 0);
 		for (int i = 0; i < outputs.length; i++) {
 			config.addEvent(new FirmEvent(FIRMS_PER_TYPE, "firm_" + i, new Endowment(new IStock[] { new Stock(SimConfig.MONEY, 1000) }, new IStock[] {}),
-					prodWeights.createProdFun(i, RETURNS_TO_SCALE), PriceFactory.EXPSEARCH));
+					prodWeights.createProdFun(i, RETURNS_TO_SCALE), PriceFactory.RANDOMIZED, "0.05"));
 		}
 		for (int i = 0; i < inputs.length; i++) {
 			config.addEvent(new ConsumerEvent(CONSUMERS_PER_TYPE, "cons_" + i, new Endowment(new Stock(inputs[i], HOURS_PER_DAY)), consWeights.createUtilFun(i, 0)));
