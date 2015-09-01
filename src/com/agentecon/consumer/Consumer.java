af729a180a936f1191577f51d97223946270e03c
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-01 22:34:01 +0200

    even more money hiding

--- src/com/agentecon/consumer/Consumer.java
+++ src/com/agentecon/consumer/Consumer.java
@@ -61,7 +61,7 @@ public class Consumer extends Agent implements IConsumer {
 		
 		// TEMP hide 50% of cash
 		IStock money = getMoney();
-		inv = inv.hide(money.getGood(), money.getAmount() * 0.5);
+		inv = inv.hide(money.getGood(), money.getAmount() * 0.8);
 		assert !AGING;
 		
 		double cash = money.getAmount();
