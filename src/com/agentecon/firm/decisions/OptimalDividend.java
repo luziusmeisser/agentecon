7946292b82ef5d34cabba2fe7640f92a050083d6
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-02 09:39:08 +0200

    constant dividends

--- src/com/agentecon/firm/decisions/OptimalDividend.java
+++ src/com/agentecon/firm/decisions/OptimalDividend.java
@@ -7,7 +7,7 @@ public class OptimalDividend implements IFirmDecisions {
 	private boolean excessMoney = false;
 
 	public double calcDividend(double cash, double profits) {
-		double adjustedProfits = profits + (excessMoney ? 0.1 : -0.1); // TEMP
+		double adjustedProfits = profits + (excessMoney ? 1 : -1);
 		return Math.min(adjustedProfits, cash / 2);
 	}
 
