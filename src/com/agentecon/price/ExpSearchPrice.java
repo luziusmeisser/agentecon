273045ee81e820820e9c20e6e81caee2055e0c9e
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-02 22:02:49 +0200

    0.1% again

--- src/com/agentecon/price/ExpSearchPrice.java
+++ src/com/agentecon/price/ExpSearchPrice.java
@@ -5,7 +5,7 @@ import com.agentecon.stats.Numbers;
 public class ExpSearchPrice extends AdaptablePrice {
 
 	public static final double MAX_ADAPTION_FACTOR = 0.5;
-	public static final double MIN_ADAPTION_FACTOR = Numbers.EPSILON * 10000;
+	public static final double MIN_ADAPTION_FACTOR = Numbers.EPSILON * 1000;
 
 	private double speed;
 	private double delta;
