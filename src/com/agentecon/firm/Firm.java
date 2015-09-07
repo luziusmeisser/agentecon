3d9082bc8e53877729ed6f02db5efde4b2b30b58
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-07 13:45:22 +0200

    fractional dividends (cash - 800)

--- src/com/agentecon/firm/Firm.java
+++ src/com/agentecon/firm/Firm.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import com.agentecon.agent.Agent;
 import com.agentecon.agent.Endowment;
 import com.agentecon.api.IFirm;
+import com.agentecon.firm.decisions.FractionalDividends;
 import com.agentecon.firm.decisions.IFirmDecisions;
 import com.agentecon.firm.decisions.StandardStrategy;
 import com.agentecon.firm.production.IPriceProvider;
@@ -31,7 +32,7 @@ public class Firm extends Agent implements IFirm, IPriceProvider {
 	protected IPriceFactory prices;
 
 	private double profits;
-	private IFirmDecisions strategy = new StandardStrategy();
+	private IFirmDecisions strategy = new FractionalDividends();
 
 	public Firm(String type, Endowment end, IProductionFunction prod, IPriceFactory prices) {
 		super(type, end);
