5ca69233fca621097149a4a77172e376fb9492f3
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-11 17:02:23 +0200

    rewind

--- src/com/agentecon/firm/decisions/FractionalDividends.java
+++ src/com/agentecon/firm/decisions/FractionalDividends.java
@@ -1,37 +1,20 @@
 package com.agentecon.firm.decisions;
 
 /**
- * Choose spendings to maximize profits. Pay out a constant fraction of cash holdings as dividend, thereby implicitely setting price levels.
+ * Choose spendings to maximize profits.
+ * Pay out a constant fraction of cash holdings as dividend, thereby implicitely setting price levels.
  */
 public class FractionalDividends implements IFirmDecisions {
 
 	public static double DIVIDEND_RATE = 0.1;
 
-	private double maxprofits = 10;
-
 	public double calcDividend(double cash, double profits) {
-		maxprofits = Math.max(profits, maxprofits);
-		double div = cash - 800;
-		if (div > 0) {
-			if (profits < maxprofits) {
-				return div; //profits / maxprofits * div;
-			} else {
-				return div;
-			}
-		} else {
-			return 0;
-		}
-//		return mix(cash, profits, 5);
-		// return cash - 800; // TEMP
+		return cash - 800; // TEMP
 	}
 
-	private double mix(double small, double large, int totweight) {
-		return (small + large * (totweight - 1)) / totweight;
-	}
-
-	public double calcCogs(double cash, double idealCogs) {
+	public double calcCogs(double cash, double idealCogs){
 		double budget = cash * 0.5;
-		if (idealCogs < budget) {
+		if (idealCogs < budget){
 			return idealCogs;
 		} else {
 			return budget;
