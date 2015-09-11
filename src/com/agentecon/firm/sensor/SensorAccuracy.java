0242faa011e217b2a6fc77c38fc272a0ab065465
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-11 16:08:39 +0200

    reduced max sensor accuracy to 1%

--- src/com/agentecon/firm/sensor/SensorAccuracy.java
+++ src/com/agentecon/firm/sensor/SensorAccuracy.java
@@ -2,7 +2,7 @@ package com.agentecon.firm.sensor;
 
 public class SensorAccuracy {
 	
-	public static final double MIN = 0.001;
+	public static final double MIN = 0.01;
 	public static final double MAX = 0.5;
 	public static final double DEFAULT = MAX / 2;
 	
@@ -27,6 +27,8 @@ public class SensorAccuracy {
 	public void lessAccurate() {
 		accuracy = Math.min(MAX, accuracy * 2);
 	}
+	
+	int count = 0;
 
 	public void moreAccurate() {
 		accuracy = Math.max(MIN, accuracy / 1.005);		
