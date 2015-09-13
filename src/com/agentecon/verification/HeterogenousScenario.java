ae814c3a99bcd8df2b0baf1a8a4af53595d14183
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-13 22:10:55 +0200

    actually one by one

--- src/com/agentecon/verification/HeterogenousScenario.java
+++ src/com/agentecon/verification/HeterogenousScenario.java
@@ -2,16 +2,9 @@ package com.agentecon.verification;
 
 import java.util.Random;
 
-import com.agentecon.agent.Endowment;
 import com.agentecon.consumer.Consumer;
 import com.agentecon.consumer.LogUtil;
-import com.agentecon.events.ConsumerEvent;
-import com.agentecon.events.FirmEvent;
-import com.agentecon.events.UpdatePreferencesEvent;
-import com.agentecon.firm.production.IProductionFunction;
-import com.agentecon.good.IStock;
-import com.agentecon.good.Stock;
-import com.agentecon.price.PriceConfig;
+import com.agentecon.events.SimEvent;
 import com.agentecon.sim.config.SimConfig;
 import com.agentecon.world.IWorld;
 
@@ -23,37 +16,45 @@ public class HeterogenousScenario extends StolperSamuelson {
 		this.rand = new Random(1313);
 	}
 
-//	@Override
-//	protected void addConsumers(int scale, SimConfig config) {
-//		for (int i = 0; i < inputs.length; i++) {
-//			Endowment end = new Endowment(new Stock(inputs[i], HOURS_PER_DAY));
-//			LogUtil util = consWeights.createUtilFun(i, 0).wiggle(rand);
-//			config.addEvent(new ConsumerEvent(scale * CONSUMERS_PER_TYPE, "cons_" + i, end, util));
-//		}
-//	}
+	// @Override
+	// protected void addConsumers(int scale, SimConfig config) {
+	// for (int i = 0; i < inputs.length; i++) {
+	// Endowment end = new Endowment(new Stock(inputs[i], HOURS_PER_DAY));
+	// LogUtil util = consWeights.createUtilFun(i, 0).wiggle(rand);
+	// config.addEvent(new ConsumerEvent(scale * CONSUMERS_PER_TYPE, "cons_" + i, end, util));
+	// }
+	// }
 
 	@Override
 	protected void addSpecialEvents(SimConfig config) {
 		int time = 2000;
-		for (int i=0; i<1000; i++){
-//		for (double current = LOW; current < HIGH; current += 0.01) {
-//			final double c2 = current;
-			config.addEvent(new UpdatePreferencesEvent(time) {
-				
-				@Override
-				public void execute(IWorld sim) {
-					update(sim.getConsumers().getRandomConsumer());
-//					for (Consumer c : sim.getConsumers().getRandomConsumers(getCardinality())) {
-//						update(c);
-//					}
-				}
+		for (int i = 0; i < 1000; i++) {
+			// for (double current = LOW; current < HIGH; current += 0.01) {
+			// final double c2 = current;
+			config.addEvent(new SimEvent(time, -1) {
 
 				@Override
-				protected void update(com.agentecon.consumer.Consumer c) {
+				public void execute(IWorld sim) {
+					for (Consumer c : sim.getConsumers().getRandomConsumers()) {
+						if (update(c)) {
+							return;
+						}
+					}
+					// for (Consumer c : sim.getConsumers().getRandomConsumers(getCardinality())) {
+					// update(c);
+					// }
+				}
+
+				protected boolean update(com.agentecon.consumer.Consumer c) {
 					LogUtil util = (LogUtil) c.getUtilityFunction();
-					util = consWeights.createDeviation(util, outputs[0], LOW);
-					util = consWeights.createDeviation(util, outputs[1], HIGH);
-					c.setUtilityFunction(util);
+					if (util.getWeights()[0] == HIGH) {
+						util = consWeights.createDeviation(util, outputs[0], LOW);
+						util = consWeights.createDeviation(util, outputs[1], HIGH);
+						c.setUtilityFunction(util);
+						return true;
+					} else {
+						return false;
+					}
 				}
 
 			});
@@ -61,15 +62,15 @@ public class HeterogenousScenario extends StolperSamuelson {
 		}
 	}
 
-//	@Override
-//	protected void addFirms(PriceConfig pricing, int scale, SimConfig config) {
-//		for (int i = 0; i < outputs.length; i++) {
-//			Endowment end = new Endowment(new IStock[] { new Stock(SimConfig.MONEY, 1000) }, new IStock[] {});
-//			for (int f = 0; f < scale * FIRMS_PER_TYPE; f++) {
-//				IProductionFunction prodfun = prodWeights.createProdFun(i, RETURNS_TO_SCALE - 0.05 + rand.nextDouble() / 10);
-//				config.addEvent(new FirmEvent(1, "firm_" + i, end, prodfun, pricing));
-//			}
-//		}
-//	}
+	// @Override
+	// protected void addFirms(PriceConfig pricing, int scale, SimConfig config) {
+	// for (int i = 0; i < outputs.length; i++) {
+	// Endowment end = new Endowment(new IStock[] { new Stock(SimConfig.MONEY, 1000) }, new IStock[] {});
+	// for (int f = 0; f < scale * FIRMS_PER_TYPE; f++) {
+	// IProductionFunction prodfun = prodWeights.createProdFun(i, RETURNS_TO_SCALE - 0.05 + rand.nextDouble() / 10);
+	// config.addEvent(new FirmEvent(1, "firm_" + i, end, prodfun, pricing));
+	// }
+	// }
+	// }
 
 }
