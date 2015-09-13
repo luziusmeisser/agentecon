3aa572ecd71835ba155f5b617b4efe8199b435d5
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-13 10:23:03 +0200

    no message

---
+++ src/com/agentecon/price/ExpSearchPrice.java
@@ -0,0 +1,46 @@
+package com.agentecon.price;
+
+import com.agentecon.stats.Numbers;
+
+public class ExpSearchPrice extends AdaptablePrice {
+
+	public static final double MAX_ADAPTION_FACTOR = 0.5;
+	public static final double MIN_ADAPTION_FACTOR = Numbers.EPSILON * 1000;
+
+	private double speed;
+	private double delta;
+	private boolean increasetm2, increasetm1;
+
+	public ExpSearchPrice(double initialDelta, double initialPrice) {
+		super(initialPrice);
+		this.delta = initialDelta;
+		this.increasetm1 = true;
+		this.increasetm2 = true;
+		this.speed = 1.1;
+	}
+
+	public ExpSearchPrice(double initialDelta) {
+		this.delta = initialDelta;
+		this.increasetm1 = true;
+		this.increasetm2 = true;
+		this.speed = 1.1;
+	}
+
+	@Override
+	protected double getFactor(boolean increasetm0) {
+		if (increasetm0 == increasetm1 && increasetm1 == increasetm2) {
+			delta = Math.min(MAX_ADAPTION_FACTOR, delta * speed);
+		} else if (increasetm0 != increasetm1 && increasetm1 != increasetm2) {
+			delta = Math.max(MIN_ADAPTION_FACTOR, delta / speed);
+		}
+		increasetm2 = increasetm1;
+		increasetm1 = increasetm0;
+		return 1.0 + delta;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString(); // + " at factor " + factor;
+	}
+
+}
--- src/com/agentecon/price/ExpSearchPrice2.java
+++ src/com/agentecon/price/ExpSearchPrice2.java
@@ -1,48 +1,46 @@
 package com.agentecon.price;
 
 import com.agentecon.stats.Numbers;
+import com.agentecon.util.MovingAverage;
 
 public class ExpSearchPrice2 extends AdaptablePrice {
 
+	public static final double MEMORY = 0.5;
+	public static final double THRESHOLD = 0.2;
+	
 	public static final double MAX_ADAPTION_FACTOR = 0.5;
 	public static final double MIN_ADAPTION_FACTOR = Numbers.EPSILON * 1000;
 
 	private double speed;
 	private double delta;
-	private boolean increasetm2, increasetm1;
+	private boolean prevIncrease;
+	private MovingAverage avg;
 
 	public ExpSearchPrice2(double initialDelta, double initialPrice) {
 		super(initialPrice);
 		this.delta = initialDelta;
-		this.increasetm1 = true;
-		this.increasetm2 = true;
 		this.speed = 1.1;
+		this.prevIncrease = false;
+		this.avg = new MovingAverage(0.5);
 	}
 
 	public ExpSearchPrice2(double initialDelta) {
 		this.delta = initialDelta;
-		this.increasetm1 = true;
-		this.increasetm2 = true;
 		this.speed = 1.1;
+		this.prevIncrease = false;
+		this.avg = new MovingAverage(0.5);
 	}
 
-	static int increase;
-	static int decrease;
-	static int total;
-
 	@Override
-	protected double getFactor(boolean increasetm0) {
-		total++;
-		if (increasetm0 == increasetm1 && increasetm1 == increasetm2) {
-			increase++;
+	protected double getFactor(boolean increase) {
+		boolean same = prevIncrease == increase;
+		avg.add(same ? 1.0 : 0.0);
+		if (avg.getAverage() > 0.8){
 			delta = Math.min(MAX_ADAPTION_FACTOR, delta * speed);
-		} else if (increasetm0 != increasetm1 && increasetm1 != increasetm2) {
-			decrease++;
+		} else if (avg.getAverage() < 0.2){
 			delta = Math.max(MIN_ADAPTION_FACTOR, delta / speed);
 		}
-		System.out.println(increase + ", " + decrease + " of " + total);
-		increasetm2 = increasetm1;
-		increasetm1 = increasetm0;
+		prevIncrease = increase;
 		return 1.0 + delta;
 	}
 
