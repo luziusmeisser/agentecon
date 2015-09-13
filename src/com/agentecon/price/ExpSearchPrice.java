952cedec80e5c541ef1463c0b19dd66f874ad52c
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-13 11:27:22 +0200

    expsearch2 moving average

--- src/com/agentecon/price/ExpSearchPrice.java
+++ src/com/agentecon/price/ExpSearchPrice.java
@@ -8,20 +8,27 @@ public class ExpSearchPrice extends AdaptablePrice {
 	public static final double MIN_ADAPTION_FACTOR = Numbers.EPSILON * 1000;
 
 	private double speed;
-	private double delta;
+	private double delta, delta2;
 	private boolean direction;
 	private int sameDirectionInARow;
+	private boolean increasetm2, increasetm1;
 
 	public ExpSearchPrice(double initialDelta, double initialPrice) {
 		super(initialPrice);
 		this.delta = initialDelta;
+		this.delta2 = delta;
 		this.sameDirectionInARow = 0;
+		this.increasetm2 = true;
+		this.increasetm1 = false;
 		this.speed = 1.1;
 	}
 
 	public ExpSearchPrice(double initialDelta) {
 		this.delta = initialDelta;
+		this.delta2 = delta;
 		this.sameDirectionInARow = 0;
+		this.increasetm2 = true;
+		this.increasetm1 = false;
 		this.speed = 1.1;
 	}
 
@@ -37,8 +44,22 @@ public class ExpSearchPrice extends AdaptablePrice {
 			direction = increase;
 			delta = Math.max(MIN_ADAPTION_FACTOR, delta / speed);
 		}
-		return 1.0 + delta;
+		double f = 1.0 + delta;
+//		double f2 = getFactor2(increase);
+//		assert f == f2;
+		return f;
 	}
+	
+//	protected double getFactor2(boolean increasetm0) {
+//		if (increasetm0 == increasetm1 && increasetm1 == increasetm2) {
+//			delta2 = Math.min(MAX_ADAPTION_FACTOR, delta2 * speed);
+//		} else if (increasetm0 != increasetm1) {
+//			delta2 = Math.max(MIN_ADAPTION_FACTOR, delta2 / speed);
+//		}
+//		increasetm2 = increasetm1;
+//		increasetm1 = increasetm0;
+//		return 1.0 + delta2;
+//	}
 
 	@Override
 	public String toString() {
