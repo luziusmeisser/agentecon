8b79a67718ecab4bd8944b1af3df34a46976e54b
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-14 20:34:33 +0200

    CogsDividend comparison

--- src/com/agentecon/firm/decisions/CogsDividend.java
+++ src/com/agentecon/firm/decisions/CogsDividend.java
@@ -23,19 +23,19 @@ public class CogsDividend implements IFirmDecisions {
 		case 1:
 			return metrics.getLatestCogs();				// 55.22325880317713
 		case 2:
-			return metrics.getIdealCogs();				// 45.55217246381391
+			return metrics.getIdealCogs();				// 45.55217246381391 Blows up
 		case 3:
-			return metrics.getLatestRevenue() / 2;		// 55.2233039078274
+			return metrics.getLatestRevenue() / 2;		// 55.2233039078274	Lowest peak
 		case 4:
 			return metrics.getExpectedRevenue() / 2;	// 55.22332350825784
 		case 5:
-			return metrics.getLatestRevenue() - metrics.getLatestCogs();	// 54.83194273768103
+			return metrics.getLatestRevenue() - metrics.getLatestCogs();	// 54.83194273768103 Prices through the roof
 		case 6:
-			return metrics.getExpectedRevenue() - metrics.getLatestCogs();	// 50.88448137265661
+			return metrics.getExpectedRevenue() - metrics.getLatestCogs();	// 50.88448137265661 Lots of micro-volatility
 		case 7:
-			return metrics.getExpectedRevenue() - metrics.getIdealCogs();	// 55.22338764543314
+			return metrics.getExpectedRevenue() - metrics.getIdealCogs();	// 55.22338764543314 Very moderate peaks
 		case 8:
-			return metrics.getIdealCogs() * 2 - metrics.getLatestCogs();	// 45.55217613220216
+			return metrics.getIdealCogs() * 2 - metrics.getLatestCogs();	// 45.55217613220216 Blows up
 		}
 	}
 
