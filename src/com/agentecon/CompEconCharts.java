f215aae93d31fead10a89a6cec7e63639bced5de
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-09-16 20:17:42 +0200

    double shock

--- src/com/agentecon/CompEconCharts.java
+++ src/com/agentecon/CompEconCharts.java
@@ -65,7 +65,7 @@ public class CompEconCharts implements IConfiguration {
 		SimConfig config = ss.createConfiguration(priceConfig, rounds);
 		for (int step = 500; step < 5000; step += 1000) {
 			ss.enableShock(config, step, 3.0);
-			ss.enableShock(config, step + 500, 7.0);
+			ss.enableShock(config, step + 20, 7.0);
 		}
 		return config;
 	}
