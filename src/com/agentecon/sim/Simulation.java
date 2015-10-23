fcef485a2a6711c3aa356bf550915f10318562b6
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-10-23 12:11:38 +0200

    retry

--- src/com/agentecon/sim/Simulation.java
+++ src/com/agentecon/sim/Simulation.java
@@ -46,7 +46,7 @@ public class Simulation implements ISimulation, IIteratedSimulation {
 	}
 
 	public Simulation() {
-		this(new StolperSamuelson().createConfiguration(PriceConfig.DEFAULT, 2000));
+		this(new StolperSamuelson().createConfiguration(PriceConfig.DEFAULT, 3000));
 	}
 	
 	public Simulation(IConfiguration metaConfig) {
