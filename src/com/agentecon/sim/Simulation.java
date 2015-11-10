6f614150854261cfdcb9038458ef16df6b3ec5c3
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-11-10 23:18:24 +0100

    no message

--- src/com/agentecon/sim/Simulation.java
+++ src/com/agentecon/sim/Simulation.java
@@ -53,7 +53,7 @@ public class Simulation implements ISimulation {
 	}
 
 	public Simulation() {
-		this(new ComparisonConfiguration(10, 100).createConfig(1, 2));
+		this(new ComparisonConfiguration(10, 100).createConfig(1, 1));
 	}
 
 	public Simulation(SimulationConfig config) {
