b96e29882fc7e1111269a6336b80657ec2c7526d
Luzius Meisser <luzius.meisser+git@gmail.com>
2015-11-11 10:58:00 +0100

    updated readme and output for savings15

--- src/com/agentecon/consumer/Consumer.java
+++ src/com/agentecon/consumer/Consumer.java
@@ -67,6 +67,7 @@ public class Consumer extends Agent implements IConsumer, IStockMarketParticipan
 				listeners.notifyDivested(this, amount);
 			} else {
 				double invest = dailySpendings.getAverage() / maxAge * (maxAge - getRetirementAge());
+				invest *= 1.5;
 				double dividendIncome = portfolio.getLatestDividendIncome();
 				if (dividendIncome < invest) {
 					savingsTarget = invest - dividendIncome;
